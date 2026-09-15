9bf17a1ad2b5fd6051a3f2e5951723fd4f0f00ef
Himalian <Voltage15312@outlook.com>
2026-09-15 21:51:56 +0800

    refactor: extract to GetApps()

--- commands/list.cs
+++ commands/list.cs
@@ -3,57 +3,64 @@ namespace Coop.Commands;
 
 public static class ListCommand
 {
-    private static string GetScoopPath()
-    {
-        string? path = Environment.GetEnvironmentVariable("SCOOP");
-        if (path == null)
-        {
-            Console.WriteLine("Warning: environment variable 'SCOOP' not found, fallback to default path");
-            path = Path.Join(Environment.GetEnvironmentVariable("HOMEPATH"), "scoop");
-        }
-        return path;
-    }
+	private static string GetScoopPath()
+	{
+		string? path = Environment.GetEnvironmentVariable("SCOOP");
+		if (path == null)
+		{
+			Console.WriteLine("Warning: environment variable 'SCOOP' not found, fallback to default path");
+			path = Path.Join(Environment.GetEnvironmentVariable("HOMEPATH"), "scoop");
+		}
+		return path;
+	}
 
-    private static string[] GetApps()
-    {
-        throw new NotImplementedException();
-    }
-    public static Command GetCommand()
-    {
-		Argument<string?> queryArgument = new("query"){
+	private static string[]? GetApps(string scoopPath, string? query)
+	{
+		string[]? apps = Directory.GetDirectories(Path.Join(scoopPath, "apps"));
+		if (!string.IsNullOrEmpty(query))
+		{
+			apps = [.. apps
+				.Select(Path.GetFileName)
+				.OfType<string>()
+				.Where(x => x != null && x.Contains(query, StringComparison.OrdinalIgnoreCase))];
+		}
+		return apps;
+	}
+	public static Command GetCommand()
+	{
+		Argument<string?> queryArgument = new("query")
+		{
 			Arity = ArgumentArity.ZeroOrOne
 		};
-        var command = new Command("list", "list installed apps")
+		var command = new Command("list", "list installed apps")
 		{
 			queryArgument
 		};
-        command.SetAction(parseResult =>
-        {
+		command.SetAction(parseResult =>
+		{
 			string? query = parseResult.GetValue(queryArgument);
-            string scoopPath = GetScoopPath();
+			string scoopPath = GetScoopPath();
 
 
-            string[] apps = Directory.GetDirectories(Path.Join(scoopPath, "apps"));
-            if (!string.IsNullOrEmpty(query))
-            {
-                Console.WriteLine($"Installed apps matching '{query}':");
-                var filteredApps = apps.Select(Path.GetFileName).Where(x => x != null && x.Contains(query,StringComparison.OrdinalIgnoreCase));
-				foreach (var app in filteredApps){
-					Console.WriteLine(Path.GetFileName(app));
-				}
-            }
-            else
-            {
-                Console.WriteLine("Installed apps:");
-                foreach (var app in apps)
-                {
-                    Console.WriteLine(app.TrimStart(Path.Join(scoopPath, "apps")));
-                }
-            }
-        });
+			var apps = GetApps(scoopPath, query);
+			if (!string.IsNullOrEmpty(query))
+			{
+				Console.WriteLine($"Installed apps matching '{query}':");
+			}
+			else
+			{
+				Console.WriteLine("Installed apps:");
+			}
+			if (apps == null) { return; }
+			foreach (var app in apps)
+			{
+				Console.WriteLine(app.TrimStart(Path.Join(scoopPath, "apps")));
+			}
+
+		});
 
 
-        return command;
+		return command;
 
-    }
+	}
 }
