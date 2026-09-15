867967822666fc9a4f7e557b3cab7982f1f1861e
Himalian <Voltage15312@outlook.com>
2026-09-15 12:05:48 +0800

    feat: basic cli structure

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+var listCommand = new Command("list", "list installed apps");
+
+listCommand.SetAction(parseResult => {
+		Console.WriteLine("Example command");
+		});
+
+var rootCommand = new RootCommand("coop - A fast Scoop alternative"){
+	listCommand
+};
+
+return await rootCommand.Parse(args).InvokeAsync();
