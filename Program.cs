03b9dde9c5d0fa289482df2ab6fea63c273e4be3
Himalian <Voltage15312@outlook.com>
2026-09-15 12:22:45 +0800

    feat: simple list command

--- Program.cs
+++ Program.cs
@@ -2,12 +2,18 @@
 
 var listCommand = new Command("list", "list installed apps");
 
-listCommand.SetAction(parseResult => {
-		Console.WriteLine("Example command");
-		});
+listCommand.SetAction(parseResult =>
+{
+    string scoopPath = Environment.GetEnvironmentVariable("SCOOP")!;
+    string[] apps = Directory.GetDirectories(Path.Join(scoopPath, "apps"));
+	Console.WriteLine("Installed apps:");
+	foreach(var app in apps){
+		Console.WriteLine(app.TrimStart(Path.Join(scoopPath,"apps")));
+	}
+});
 
 var rootCommand = new RootCommand("coop - A fast Scoop alternative"){
-	listCommand
+    listCommand
 };
 
 return await rootCommand.Parse(args).InvokeAsync();
